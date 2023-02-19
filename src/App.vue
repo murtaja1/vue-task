2778d2ac551390854fbe76216457551b84b754eb
murtaja1 <murtajaadnan7@gmail.com>
2023-02-19 22:22:51 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container">
+		<div  >
+			<TabsComp :tabs="tabs" />
+
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TabsComp from "./components/TabsComp.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		TabsComp,
+	},
+	data() {
+		return {
+      showBodyTitle:'',
+			tabs: [
+				{
+					title: "Info",
+					body: "Info: Design the following tabs component using HTML, CSS, and vanilla JavaScript",
+				},
+				{ title: "Phone", body: "Phone: 07889883474" },
+				{ title: "Email", body: "EmailL: murtajaadnan@gmail.com" },
+				{ title: "Github", body: "Github: https://murtaja@github" },
+				{ title: "LinkedIn", body: "LinkedIn: https://murtaja@LinkedIn" },
+			],
+		};
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
+
 </style>
---
+++ src/components/TabsComp.vue
@@ -0,0 +1,60 @@
+<template lang="">
+	<div class="container">
+		<div
+			:key="tab.title"
+			v-for="(tab, index) in tabs"
+			@click="handleClick(index)"
+		>
+			<span class="tab" :class="[index == id ? 'selected' : '', 'tab']">{{
+				tab.title
+			}}</span>
+			<span v-show="index != 4" class="divider"></span>
+		</div>
+	</div>
+	<body class="textBody">
+		{{ tabs[id].body }}
+	</body>
+</template>
+<script>
+export default {
+	name: "TabsComp",
+	props: {
+		tabs: Array,
+	},
+	data() {
+		return {
+			id: 0,
+		};
+	},
+	methods: {
+		handleClick(id) {
+			this.id = id;
+		},
+	},
+};
+</script>
+<style lang="css" scoped>
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding-left: 100px;
+}
+.tab {
+	padding: 15px;
+	border-bottom: solid black 2px;
+	display: inline-block;
+	cursor: pointer;
+}
+.divider {
+	border-left: 2px solid black;
+	height: 500px;
+}
+.selected {
+	background: blue;
+	color: white;
+}
+.textBody {
+	margin-top: 30px;
+}
+</style>
